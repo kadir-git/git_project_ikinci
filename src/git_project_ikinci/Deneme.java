1fa45364f0bf9670ab678972d5edda938d7a8bf6
kadir-git <69384728+kadir-git@users.noreply.github.com>
2020-08-09 22:19:21 +0200

    Update Deneme.java
    github'da degisiklik yaptim

--- src/git_project_ikinci/Deneme.java
+++ src/git_project_ikinci/Deneme.java
@@ -5,7 +5,10 @@ public class Deneme {
 	public static void main(String[] args) {
 		
 		System.out.println("Ikinci git projem.");
+		
 		System.out.println("ikinci satir Code'um");
+		
+		System.out.println(remote degisiklik");
 
 	}
 
